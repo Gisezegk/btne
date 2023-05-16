fefc5a8b8dedf12cee42ba60fd4aa374dc0c3973
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-05-16 01:11:57 -0700

    task 1 fixed maybe

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,39 +10,35 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node, *tmp;
+	binary_tree_t *node;
 
 	/*chack if parent is NULL*/
-	if (parent == NULL)
+	if (parent != NULL)
 	{
-		return (NULL);
-	}
+		/* Allocate memory for new node */
+		node = malloc(sizeof(binary_tree_t));
 
-	/* Allocate memory for new node */
-	node = malloc(sizeof(binary_tree_t));
-	if
-		(node == NULL)
-	{
-		return (NULL);
-	}
+		if (node == NULL)
+		{
+			return (NULL);
+		}
 
-	/* Initialize node's members */
+		/* Initialize node's members */
 
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
+		node->n = value;
+		node->parent = parent;
+		node->left = NULL;
 
-	/* Set parent's left-child */
-	if (parent->left != NULL)
-	{
-		tmp = parent->left;
+		if (parent->left != NULL)
+		{
+			parent->left->parent = node;
+			node->left = parent->left;
+		}
+
 		parent->left = node;
-		node->left = tmp;
-		tmp->parent = node;
+
+		return (node);
 	}
-	else
-	{
-		parent->left = node;
-	}
-	return (node);
+
+	return (0);
 }
