8fa848c5ca57b83fdd8cc3f45b151edd9c3d22b4
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-05-16 01:16:05 -0700

    now i hope that task 1 was fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 		if (node == NULL)
 		{
+			free(node);
 			return (NULL);
+
 		}
 
 		/* Initialize node's members */
@@ -28,6 +30,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		node->n = value;
 		node->parent = parent;
 		node->left = NULL;
+		node->right = NULL;
 
 		if (parent->left != NULL)
 		{
