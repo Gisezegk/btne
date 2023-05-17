6f7b5470029b21c292092419e4758457894a9a94
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-05-16 19:01:50 -0700

    18 uncle mistakes fixed

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,13 +1,19 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+binary_tree_t
+*binary_tree_uncle(binary_tree_t *node)
 {
+
+	binary_tree_t *parent;
+
+	binary_tree_t *grandparent;
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = parent->parent;
+		parent = node->parent;
+		grandparent = parent->parent;
 
 	if (grandparent->left == parent)
 	{
